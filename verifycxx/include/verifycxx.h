5a7886b4b4459b3b0c29686db445e7a706bdf511
nevergiveup-c <217945237+nevergiveup-c@users.noreply.github.com>
2025-10-24 15:28:42 +0700

    refactor(verifycxx): redesign classes

--- verifycxx/include/verifycxx.h
+++ verifycxx/include/verifycxx.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
 #include <utility>
+#include <type_traits>
 
 #if defined(__aarch64__) || defined(_M_ARM64) || defined(__ARM_NEON)
 #include <arm_neon.h>
@@ -37,23 +38,53 @@ struct verifycxx_header {
     bool is_valid() const noexcept { return u.bits.magic == VDH_MAGIC; }
 };
 
-template <class Type> class verifycxx : public verifycxx_header {
+template <class Type> class verifycxx;
+
+template <typename Type> class verifycxx_modify_guard {
 public:
-    class modify_guard {
-    public:
-        explicit modify_guard(verifycxx& parent) :
-            parent(parent) {
-        }
+    explicit verifycxx_modify_guard(verifycxx<Type>& parent) : parent(parent) {}
+    ~verifycxx_modify_guard() { parent.update_checksum(); }
 
-        ~modify_guard() { parent.update_checksum(); }
-        Type* operator->() { return parent.data; }
-        Type& operator*() { return *parent.data; }
-        Type& operator=(const Type& rhs) { return *parent.data = rhs; }
+    Type* operator->() { return parent.data; }
+    Type& operator*() { return *parent.data; }
 
-    private:
-        verifycxx& parent;
-    };
+    Type& operator=(const Type& rhs) { return *parent.data = rhs; }
 
+    Type& operator+=(const Type& rhs) { *parent.data += rhs; return *parent.data; }
+    Type& operator-=(const Type& rhs) { *parent.data -= rhs; return *parent.data; }
+    Type& operator*=(const Type& rhs) { *parent.data *= rhs; return *parent.data; }
+    Type& operator/=(const Type& rhs) { *parent.data /= rhs; return *parent.data; }
+    Type& operator%=(const Type& rhs) { *parent.data %= rhs; return *parent.data; }
+
+    Type& operator&=(const Type& rhs) { *parent.data &= rhs; return *parent.data; }
+    Type& operator|=(const Type& rhs) { *parent.data |= rhs; return *parent.data; }
+    Type& operator^=(const Type& rhs) { *parent.data ^= rhs; return *parent.data; }
+    Type& operator<<=(const Type& rhs) { *parent.data <<= rhs; return *parent.data; }
+    Type& operator>>=(const Type& rhs) { *parent.data >>= rhs; return *parent.data; }
+
+    Type& operator++() { ++(*parent.data); return *parent.data; }
+    Type operator++(int) { return (*parent.data)++; }
+    Type& operator--() { --(*parent.data); return *parent.data; }
+    Type operator--(int) { return (*parent.data)--; }
+
+    Type operator+(const Type& rhs) const { return *parent.data + rhs; }
+    Type operator-(const Type& rhs) const { return *parent.data - rhs; }
+    Type operator*(const Type& rhs) const { return *parent.data * rhs; }
+    Type operator/(const Type& rhs) const { return *parent.data / rhs; }
+    Type operator%(const Type& rhs) const { return *parent.data % rhs; }
+
+    template<typename T = Type> auto& operator[](size_t i) requires requires(T t) { t[i]; } {
+        return (*parent.data)[i];
+    }
+
+private:
+    verifycxx<Type>& parent;
+};
+
+template <class Type> class verifycxx : public verifycxx_header {
+    friend class verifycxx_modify_guard<Type>;
+    using modify_guard = verifycxx_modify_guard<Type>;
+public:
     template <typename... Args>
     explicit verifycxx(Args&&... args) : verifycxx_header(sizeof(Type)) {
         if constexpr (sizeof...(Args) == 1 && std::is_scalar_v<Type>) {
@@ -67,78 +98,80 @@ public:
 
     ~verifycxx() { delete data; }
 
-    const Type* get() noexcept { return data; }
+    const Type* get() const noexcept { return data; }
     modify_guard modify() noexcept { return modify_guard(*this); }
-    FORCEINLINE bool verify() const noexcept { return checksum == gen_checksum(); }
 
+    FORCEINLINE bool verify() const noexcept { return checksum == gen_checksum(); }
     FORCEINLINE uint64_t get_checksum() const noexcept { return checksum; }
 
     operator bool() const noexcept { return verify(); }
-    operator Type() const noexcept requires std::is_scalar_v<Type> {
-        return *data;
-    }
-    operator const char*() const requires requires(Type t) { t.c_str(); } {
-        return data->c_str();
-    }
-    template<typename T = Type>
-    auto& operator[](size_t i) requires requires(T t) { t[i]; } {
-        return (*data)[i];
-    }
+
+    operator Type() const noexcept requires std::is_scalar_v<Type> { return *data; }
+    operator const char*() const requires requires(Type t) { t.c_str(); } { return data->c_str(); }
+
+    template<typename T = Type> auto& operator[](size_t i) requires requires(T t) { t[i]; } { return (*data)[i]; }
+
+    const Type& operator*() const noexcept { return *data; }
+    const Type* operator->() const noexcept { return data; }
 
     auto begin() const noexcept { return data->begin(); }
     auto end() const noexcept { return data->end(); }
 
 private:
-    FORCEINLINE uint64_t gen_checksum() const {
-        const uint16_t cookie = u.bits.cookie;
-        uint64_t sum = 0;
-
-        auto process = [&](const uint8_t* ptr, size_t size) {
-            size_t i = 0;
-
-#if defined(__aarch64__) || defined(_M_ARM64)
-            uint64x2_t neon_sum = vdupq_n_u64(0);
-            for (; i + 16 <= size; i += 16) {
-                uint8x16_t data = vld1q_u8(ptr + i);
-                uint16x8_t cookie_vec = vdupq_n_u16(cookie);
-                neon_sum = vpadalq_u16(neon_sum, veorq_u16(vmovl_u8(vget_low_u8(data)), cookie_vec));
-                neon_sum = vpadalq_u16(neon_sum, veorq_u16(vmovl_u8(vget_high_u8(data)), cookie_vec));
-            }
-            sum += vgetq_lane_u64(neon_sum, 0) + vgetq_lane_u64(neon_sum, 1);
-
-#elif defined(__clang__) || defined(__GNUC__)
-            __m128i xmm_sum = _mm_setzero_si128();
-            __m128i xmm_cookie = _mm_set1_epi16(cookie);
-            for (; i + 16 <= size; i += 16) {
-                __m128i data = _mm_loadu_si128(reinterpret_cast<const __m128i*>(ptr + i));
-                xmm_sum = _mm_add_epi64(xmm_sum, _mm_sad_epu8(_mm_xor_si128(data, xmm_cookie), _mm_setzero_si128()));
-            }
-            alignas(16) uint64_t temp[2];
-            _mm_store_si128(reinterpret_cast<__m128i*>(temp), xmm_sum);
-            sum += temp[0] + temp[1];
-
-#else
-            __m256i mm256_sum = _mm256_setzero_si256();
-            __m256i mm256_cookie = _mm256_set1_epi16(cookie);
-            for (; i + 32 <= size; i += 32) {
-                __m256i data = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(ptr + i));
-                mm256_sum = _mm256_add_epi64(mm256_sum, _mm256_sad_epu8(_mm256_xor_si256(data, mm256_cookie), _mm256_setzero_si256()));
-            }
-            alignas(32) uint64_t temp[4];
-            _mm256_store_si256(reinterpret_cast<__m256i*>(temp), mm256_sum);
-            sum += temp[0] + temp[1] + temp[2] + temp[3];
-#endif
-
-            for (; i < size; ++i) sum += ptr[i] ^ cookie;
-            };
-
-        process(reinterpret_cast<const uint8_t*>(this), sizeof(verifycxx_header));
-        process(reinterpret_cast<const uint8_t*>(data), sizeof(Type));
-
-        return sum;
-    }
+    uint64_t process_simd(const uint8_t* ptr, size_t size) const;
+    FORCEINLINE uint64_t gen_checksum() const;
     void update_checksum() { checksum = gen_checksum(); }
 
-    Type*    data{};
-    uint64_t checksum;
+    Type* data{};
+    uint64_t checksum{};
 };
+
+template<class Type> uint64_t verifycxx<Type>::process_simd(const uint8_t *ptr, size_t size) const {
+    const uint16_t cookie = u.bits.cookie;
+    uint64_t sum = 0;
+    size_t i = 0;
+
+#if defined(__aarch64__) || defined(_M_ARM64)
+        uint64x2_t neon_sum = vdupq_n_u64(0);
+        for (; i + 16 <= size; i += 16) {
+            uint8x16_t data = vld1q_u8(ptr + i);
+            uint16x8_t cookie_vec = vdupq_n_u16(cookie);
+            neon_sum = vpadalq_u16(neon_sum, veorq_u16(vmovl_u8(vget_low_u8(data)), cookie_vec));
+            neon_sum = vpadalq_u16(neon_sum, veorq_u16(vmovl_u8(vget_high_u8(data)), cookie_vec));
+        }
+        sum += vgetq_lane_u64(neon_sum, 0) + vgetq_lane_u64(neon_sum, 1);
+
+#elif defined(__clang__) || defined(__GNUC__)
+    __m128i xmm_sum = _mm_setzero_si128();
+    __m128i xmm_cookie = _mm_set1_epi16(cookie);
+    for (; i + 16 <= size; i += 16) {
+        __m128i data = _mm_loadu_si128(reinterpret_cast<const __m128i*>(ptr + i));
+        xmm_sum = _mm_add_epi64(xmm_sum, _mm_sad_epu8(_mm_xor_si128(data, xmm_cookie), _mm_setzero_si128()));
+    }
+    alignas(16) uint64_t temp[2];
+    _mm_store_si128(reinterpret_cast<__m128i*>(temp), xmm_sum);
+    sum += temp[0] + temp[1];
+
+#else
+        __m256i mm256_sum = _mm256_setzero_si256();
+        __m256i mm256_cookie = _mm256_set1_epi16(cookie);
+        for (; i + 32 <= size; i += 32) {
+            __m256i data = _mm256_loadu_si256(reinterpret_cast<const __m256i*>(ptr + i));
+            mm256_sum = _mm256_add_epi64(mm256_sum, _mm256_sad_epu8(_mm256_xor_si256(data, mm256_cookie), _mm256_setzero_si256()));
+        }
+        alignas(32) uint64_t temp[4];
+        _mm256_store_si256(reinterpret_cast<__m256i*>(temp), mm256_sum);
+        sum += temp[0] + temp[1] + temp[2] + temp[3];
+#endif
+
+    for (; i < size; ++i) sum += ptr[i] ^ cookie;
+
+    return sum;
+}
+
+template<class Type> uint64_t verifycxx<Type>::gen_checksum() const {
+    uint64_t sum = 0;
+    sum += process_simd(reinterpret_cast<const uint8_t*>(this), sizeof(verifycxx_header));
+    sum += process_simd(reinterpret_cast<const uint8_t*>(data), sizeof(Type));
+    return sum;
+}
