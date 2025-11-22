f23394036d627b8bdf51abb9dfa6cc68bc5ce08f
nevergiveup-c <217945237+nevergiveup-c@users.noreply.github.com>
2025-11-23 00:19:08 +0700

    refactor(quick_test): remove test code

--- verifycxx/quick_test.cpp
+++ verifycxx/quick_test.cpp
@@ -1,40 +1,9 @@
-#include <utility>
-
-#include "windows.h"
 #include "metrics/unit_test.h"
 #include "metrics/benchmark.h"
-
-struct Vector3 {
-    float pos[3]{};
-};
-
-struct Entity {
-    float health{ 100.f }, armour{ 100.f };
-    Vector3 position{};
-};
-
-struct Player : Entity {
-    Player(const int uid, std::string n, const int l) :
-        uniqueId(uid), name(std::move(n)), level(l) {};
-    ~Player() = default;
-
-    int uniqueId{};
-    std::string name{};
-    int level{};
-};
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     auto const result = RUN_ALL_TESTS();
-
-    verifycxx<int> value{ 1337 };
-    std::cout << &value << std::endl;
-
-    verifycxx<Player> player{ 1, "Nigger", 15 };
-    std::cout << &player << "crc: " << (uint64_t)player.get_checksum() << " " << offsetof(verifycxx<Player>, checksum) << std::endl;
-    std::cout << player->name.c_str() << std::endl;
-
-    Sleep(INFINITE);
 
     std::cout << '\n';
 
